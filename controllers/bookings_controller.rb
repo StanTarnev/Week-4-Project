b2367b3211b0e1c9870a5514f5ce00092d8f5f4f
Stan Tarnev <stan.tarnev@gmail.com>
2020-06-28 23:32:24 +0100

    amended bookings 'create' view to show message based on conditional logic

--- controllers/bookings_controller.rb
+++ controllers/bookings_controller.rb
@@ -22,20 +22,20 @@ end
 
 post '/bookings' do
   new_booking = Booking.new(params)
-  premium_membership = false
+  @premium_membership = false
   if new_booking.can_book()
-    premium_membership = true
+    @premium_membership = true
   end
 
   bookings = Booking.all()
-  member_already_booked_for_time_slot = false
+  @member_already_booked_for_time_slot = false
   for booking in bookings
     if booking.member_id == new_booking.member_id && booking.gym_class.time_slot == new_booking.gym_class.time_slot
-      member_already_booked_for_time_slot = true
+      @member_already_booked_for_time_slot = true
     end
   end
 
-  if !member_already_booked_for_time_slot && premium_membership
+  if !@member_already_booked_for_time_slot && @premium_membership
     new_booking.save()
     redirect to('/bookings')
   else
