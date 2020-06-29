670494fea46bdb9021f7400beb19e7c4e636aaf1
Stan Tarnev <stan.tarnev@gmail.com>
2020-06-29 01:12:18 +0100

    added logic preventing duplicates when updating bookings and added conditional message in the view

--- controllers/bookings_controller.rb
+++ controllers/bookings_controller.rb
@@ -51,9 +51,22 @@ get '/bookings/:id/edit' do
 end
 
 post '/bookings/:id' do
-  @booking = Booking.new(params)
-  if @booking.can_book()
-    @booking.update()
+  updated_booking = Booking.new(params)
+  @premium_membership = false
+  if updated_booking.can_book()
+    @premium_membership = true
+  end
+
+  bookings = Booking.all()
+  @member_already_booked_for_time_slot = false
+  for booking in bookings
+    if booking.member_id == updated_booking.member_id && booking.gym_class.time_slot == updated_booking.gym_class.time_slot
+      @member_already_booked_for_time_slot = true
+    end
+  end
+
+  if !@member_already_booked_for_time_slot && @premium_membership
+    updated_booking.update()
     redirect to('/bookings')
   else
     erb(:'bookings/update')
