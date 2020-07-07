7d6df200abf1be3a417e3e230209dd6a89a6683a
Stan Tarnev <stan.tarnev@gmail.com>
2020-07-07 01:14:44 +0100

    created route and form for adding gym class to member, plus logic in the 'show' route and view to show a button conditionally

--- controllers/members_controller.rb
+++ controllers/members_controller.rb
@@ -14,7 +14,43 @@ get '/members/new' do
 end
 
 get '/members/:id' do
-  @member = Member.find(params[:id])
+  all_gym_classes = GymClass.all()
+  @current_member = Member.find(params[:id])
+
+  gym_classes_with_available_places = []
+  for gym_class in all_gym_classes
+    if gym_class.capacity > gym_class.members.length
+      gym_classes_with_available_places.push(gym_class)
+    end
+  end
+
+  gym_classes_not_conflicting_in_time_slot = []
+  for gym_class in gym_classes_with_available_places
+    if @current_member.gym_classes.length > 0
+      gym_classes_conflicting_in_time_slot = []
+      @current_member.gym_classes.each do |current_member_gym_class|
+        if current_member_gym_class.time_slot == gym_class.time_slot
+          gym_classes_conflicting_in_time_slot.push(gym_class)
+        end
+      end
+      if gym_classes_conflicting_in_time_slot.length < 1
+        gym_classes_not_conflicting_in_time_slot.push(gym_class)
+      end
+    else
+      gym_classes_not_conflicting_in_time_slot.push(gym_class)
+    end
+  end
+
+  class_cannot_be_enrolled = []
+  @gym_classes_not_conflicting = []
+  for gym_class in gym_classes_not_conflicting_in_time_slot
+    if gym_class.premium_time_slot_check && !@current_member.premium_membership
+      class_cannot_be_enrolled.push(gym_class)
+    else
+      @gym_classes_not_conflicting.push(gym_class)
+    end
+  end
+
   erb(:'members/show')
 end
 
@@ -67,4 +103,45 @@ post '/members/:id/delete' do
   @member = Member.find(params[:id])
   @member.delete()
   redirect to('/members')
+end
+
+get '/members/:id/add_gym_class' do
+  all_gym_classes = GymClass.all()
+  @current_member = Member.find(params[:id])
+
+  gym_classes_with_available_places = []
+  for gym_class in all_gym_classes
+    if gym_class.capacity > gym_class.members.length
+      gym_classes_with_available_places.push(gym_class)
+    end
+  end
+
+  gym_classes_not_conflicting_in_time_slot = []
+  for gym_class in gym_classes_with_available_places
+    if @current_member.gym_classes.length > 0
+      gym_classes_conflicting_in_time_slot = []
+      @current_member.gym_classes.each do |current_member_gym_class|
+        if current_member_gym_class.time_slot == gym_class.time_slot
+          gym_classes_conflicting_in_time_slot.push(gym_class)
+        end
+      end
+      if gym_classes_conflicting_in_time_slot.length < 1
+        gym_classes_not_conflicting_in_time_slot.push(gym_class)
+      end
+    else
+      gym_classes_not_conflicting_in_time_slot.push(gym_class)
+    end
+  end
+
+  class_cannot_be_enrolled = []
+  @gym_classes_not_conflicting = []
+  for gym_class in gym_classes_not_conflicting_in_time_slot
+    if gym_class.premium_time_slot_check && !@current_member.premium_membership
+      class_cannot_be_enrolled.push(gym_class)
+    else
+      @gym_classes_not_conflicting.push(gym_class)
+    end
+  end
+
+  erb(:'members/add_gym_class')
 end
