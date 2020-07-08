ef05fc46ed156504cc7d020a773d6040be26dad0
Stan Tarnev <stan.tarnev@gmail.com>
2020-07-08 02:46:56 +0100

    premium membership now shows 'Yes/No' instead of 'true/false'

--- helpers/sql_helper.rb
+++ helpers/sql_helper.rb
@@ -1,10 +1,10 @@
 class SqlHelper
     def self.get_bool(bool)
         psql_hash = {
-            't' => true,
-            'f' => false,
-            'true' => true,
-            'false' => false
+            't' => 'Yes',
+            'f' => 'No',
+            'true' => 'Yes',
+            'false' => 'No'
         }
 
         if !!bool == bool
