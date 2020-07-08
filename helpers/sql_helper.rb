2aa7b875ba412fb5a76afabd210c3aa215598515
Stan Tarnev <stan.tarnev@gmail.com>
2020-07-08 18:17:38 +0100

    replaced 'Yes/No' for premium membership with tick/cross'

--- helpers/sql_helper.rb
+++ helpers/sql_helper.rb
@@ -1,10 +1,10 @@
 class SqlHelper
     def self.get_bool(bool)
         psql_hash = {
-            't' => 'Yes',
-            'f' => 'No',
-            'true' => 'Yes',
-            'false' => 'No'
+            't' => '✔️',
+            'f' => '❌',
+            'true' => '✔️',
+            'false' => '❌'
         }
 
         if !!bool == bool
